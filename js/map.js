19d5f1fdd87bdaded9393ade235feffd349a6052
baripembo <erikawei@gmail.com>
2018-09-14 16:45:57 -0400

    fading in icons, adding conflict data

--- js/map.js
+++ js/map.js
@@ -1,26 +1,28 @@
 $( document ).ready(function() {
-  let dataUrls = ['geodata_segment1.geojson','geodata_segment2.geojson','geodata_segment3.geojson','geodata_segment4.geojson','geodata_segment5.geojson'];
+  let dataUrls = ['geodata_segment1.geojson','geodata_segment2.geojson','geodata_segment3.geojson','geodata_segment4.geojson','geodata_segment6.geojson','geodata_segment5.geojson'];
+  let layerArray = new Array(dataUrls.length);
   let map;
+  const DATA_URL = 'https://baripembo.github.io/idp-southsudan/';
   mapboxgl.accessToken = 'pk.eyJ1IjoiaHN3OTgiLCJhIjoiY2oyOXh2dzlxMDAwYzJ3bzcyMnRseXcxNCJ9.1h5sGCIL0Pig6OmgZdDBMg';
 
 
   $('.main-content').height($('#map').height());
-  
-  let mapPositions = [0,1230,1730,2200,2744,3350];
+
+  let mapPositions = [0,1050,1550,2110,2550,3150];
   let mainHeaderPos = $('.main-header').position().top;
+  let footerPos = $('.footer').position().top;
   $(window).scroll(function (event) {
       let scrollPos = $(window).scrollTop();
-      console.log('scrollPos',scrollPos)
       //control sticky header
-      if (scrollPos > mainHeaderPos && scrollPos < 3225) {
+      if (scrollPos > mainHeaderPos && (scrollPos+$(window).height()+50) < footerPos) {
         $('.main-header').addClass('sticky');
         $('.story').addClass('sticky');
-        //$('.footer').hide();
+        let stickyPos = $('.story.sticky').css('top').replace(/[^-\d\.]/g, '');
+        $('.story').css('height', $(window).height()-stickyPos);
       }
       else {
         $('.main-header').removeClass('sticky');
         $('.story').removeClass('sticky');
-        //$('.footer').show();
       }
 
       //change text content based on position of scroll
@@ -28,14 +30,19 @@ $( document ).ready(function() {
         if (scrollPos>mapPositions[i-1] && scrollPos<=mapPositions[i]) {
           $('.segment').hide();
           $('.segment:nth-child('+i+')').show();
+
+          //show segment map icons
+          if (map.getLayer(layerArray[i]) && map.getPaintProperty(layerArray[i], 'icon-opacity') < 1) {
+            map.setPaintProperty(layerArray[i], 'icon-opacity', 1);
+          }
         }
       }
   });
 
   function getData() {
-    dataUrls.forEach(function (url) {
+    dataUrls.forEach(function (url, index) {
       loadData(url, function (responseText) {
-        parseData(JSON.parse(responseText));
+        parseData(JSON.parse(responseText), index);
       })
     })
   }
@@ -43,7 +50,7 @@ $( document ).ready(function() {
   function loadData(dataPath, done) {
       var xhr = new XMLHttpRequest();
       xhr.onload = function () { return done(this.responseText) }
-      xhr.open('GET', 'https://baripembo.github.io/idp-southsudan/data/'+dataPath, true);
+      xhr.open('GET', DATA_URL+'data/'+dataPath, true);
       xhr.send();
   }
 
@@ -82,9 +89,9 @@ $( document ).ready(function() {
     map.scrollZoom.disable();
 
     //add icon images
-    let iconArray = ['icon_circle','icon_foot','icon_boat'];
+    let iconArray = ['icon_circle','icon_foot','icon_boat','icon_clash'];
     iconArray.forEach(function(imageName) {
-      map.loadImage('https://baripembo.github.io/idp-southsudan/assets/icons/'+imageName+'.png', function(error, image) {
+      map.loadImage(DATA_URL+'assets/icons/'+imageName+'.png', function(error, image) {
         map.addImage(imageName, image);
       });
     });
@@ -92,38 +99,66 @@ $( document ).ready(function() {
     //get data
     map.on('load', function() {
       locationData();
+      conflictData();
       getData();
     });
   }
 
   function locationData() {
-    map.addSource('segment1', {
+    map.addSource('locationSource', {
       type: 'geojson',
-      data: 'https://baripembo.github.io/idp-southsudan/data/geodata_locations.geojson'
+      data: DATA_URL+'data/geodata_locations.geojson'
     });
 
     map.addLayer({
-      'id': 'points',
+      'id': 'locationPoints',
       'type': 'symbol',
-      'source': 'segment1',
+      'source': 'locationSource',
       'layout': {
         'icon-image': 'icon_circle',
-        'icon-offset': { "type": "identity", "property": "iconOffset" },
+        'icon-offset': { 'type': 'identity', 'property': 'iconOffset' },
         'text-field': '{name}',
         'text-font': ['PT Sans Bold Italic', 'Arial Unicode MS Bold'],
         'text-size': 11,
-        'text-offset': { "type": "identity", "property": "textOffset" },
-        'text-anchor': { "type": "identity", "property": "textAnchor" },
+        'text-offset': { 'type': 'identity', 'property': 'textOffset' },
+        'text-anchor': { 'type': 'identity', 'property': 'textAnchor' },
         'icon-allow-overlap': true,
         'text-allow-overlap': true
       }
     });
   }
 
-  function parseData(geoData) {
+  function conflictData() {
+    map.addSource('clashSource', {
+      type: 'geojson',
+      data: DATA_URL+'data/conflict_sample.geojson'
+    });
+
+    map.addLayer({
+      'id': 'clashPoints',
+      'type': 'symbol',
+      'source': 'clashSource',
+      'layout': {
+        'icon-image': 'icon_clash',
+        'icon-offset': { 'type': 'identity', 'property': 'iconOffset' },
+        // 'text-field': '{event_type}',
+        // 'text-font': ['PT Sans Bold Italic', 'Arial Unicode MS Bold'],
+        // 'text-size': 11,
+        // 'text-offset': { 'type': 'identity', 'property': 'textOffset' },
+        // 'text-anchor': { 'type': 'identity', 'property': 'textAnchor' },
+        'icon-allow-overlap': true,
+        'text-allow-overlap': true
+      }
+    });
+  }
+
+  function parseData(geoData, index) {
     geoData.features.forEach(function(feature) {
       let points = convertLineStringtoPoint(feature.geometry.coordinates, feature.properties.transport);
-      let source = feature.properties.name.replace(/ /g, '').toLowerCase()+'Layer';
+      let name = feature.properties.name.replace(/ /g, '').toLowerCase()
+      let source = name+'Layer';
+      layerArray[index] = source;
+      let iconOpacity = index===0? 1 : 0;
       map.addSource(source, {
         type: 'geojson',
         data: {
@@ -132,14 +167,18 @@ $( document ).ready(function() {
         }
       });
       map.addLayer({
-        'id': source+'Layer',
+        'id': source,
         'type': 'symbol',
         'source': source,
         'layout': {
           'icon-image': 'icon_{transport}',
           'icon-padding': 6
-          // 'icon-allow-overlap': true,
-          // 'text-allow-overlap': true
+        },
+        'paint': {
+          'icon-opacity': iconOpacity,
+          'icon-opacity-transition': {
+            'duration': 1000
+          }
         }
       });
     });
